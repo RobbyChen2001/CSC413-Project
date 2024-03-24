a92079e70845da02ecb30f70b0c21953e928ba4a
Irene <ireneyr.li@mail.utoronto.ca>
2024-03-24 14:12:35 -0400

    image augmentation test script

---
+++ image_augmentation.py
@@ -0,0 +1,66 @@
+import os
+import pathlib
+import re
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+
+
+zse_sbir_dir = "./ZSE-SBIR"
+datasets = {"QuickDraw": {}, "Sketchy": {}, "TUBerlin": {}}
+
+zs_dataset_dir = os.path.join(zse_sbir_dir, "datasets")
+da_dir = "./data-augmentation"
+augment_type = "gaussian-noise"
+
+for ds in datasets.keys():
+    ds_path = pathlib.Path(os.path.join(zs_dataset_dir, ds))
+    for folder in ds_path.iterdir():
+        for file in folder.iterdir():
+            # Read from the list of test sketches paths
+            if file.name.endswith("_zero.txt") and ("png_ready" in file.name or "sketch" in file.name):
+                with open(file, 'r') as f:
+                    for line in f.readlines():
+                        # Obtain the path of test sketches and add to datasets dictionary
+                        img_path_in_class = re.search(r"(.*) ", line).group(1)
+                        class_name = img_path_in_class.split("/")[-2]
+                        img_full_path = os.path.join("./dataset", ds, img_path_in_class)
+                        #Dictionary {dataset: {class: image}...}
+                        if class_name not in datasets[ds]:
+                            datasets[ds][class_name] = {img_full_path}
+                        datasets[ds][class_name].add(img_full_path)
+
+augment_dir = da_dir
+if not os.path.exists(da_dir):
+    os.makedirs(da_dir)
+    augment_dir = pathlib.Path(os.path.join(da_dir, augment_type))
+    if not os.path.exists(augment_dir):
+        os.makedirs(augment_dir)
+        for ds in datasets.keys():
+            os.makedirs(pathlib.Path(os.path.join(augment_dir, ds)))
+else:
+    augment_dir = pathlib.Path(os.path.join(da_dir, augment_type))
+
+
+for ds_name, ds_set in datasets.items():
+    ds_num = 3
+    for class_name in ds_set.keys():
+        for img_full_path in ds_set[class_name]:
+            if ds_num > 0:
+                print(ds_name, class_name, img_full_path)
+                # img = mpimg.imread(img_full_path)
+                # imgplot = plt.imshow(img)
+                # plt.show()
+
+                augmented_path = pathlib.Path(os.path.join(augment_dir, ds_name))
+                original = cv2.imread(img_full_path, cv2.IMREAD_UNCHANGED)
+                # Apply Gaussian blur
+                augmented = cv2.GaussianBlur(original, (5, 5), cv2.BORDER_DEFAULT)
+                cv2.imwrite(os.path.join(augmented_path, img_full_path.split("/")[-1]), augmented)
+                # os.chmod(os.path.join(augmented_path, img_full_path.split("/")[-1]), 777)
+                # img = mpimg.imread(augmented_path)
+                # imgplot = plt.imshow(img)
+                # plt.show()
+
+            ds_num -= 1
